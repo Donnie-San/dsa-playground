0fa8fe974c1b4e2b7c21d60e6c7a9680ec68c1a5
Domi <donniesan76@gmail.com>
2025-09-23 08:08:59 +0700

    add stack array, stack linked, and queue linked

---
+++ CoreDataStructure/StackLinked.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAPlayground.CoreDataStructure
+{
+    public class StackLinked<T>
+    {
+        private Node<T> _head;
+        private int _count;
+
+        public int Count => _count;
+        public bool IsEmpty => _head is null;
+
+        public void Push(T val)
+        {
+            Node<T> newNode = new Node<T>(val) {
+                Next = _head
+            };
+            _head = newNode;
+            _count++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Error: Stack Underflow");
+
+            T returnVal = _head.Val;
+            _head = _head.Next;
+            _count--;
+            return returnVal;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Error: Stack Undeflow");
+
+            return _head.Val;
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _count = 0;
+        }
+
+        public override string ToString()
+        {
+            var current = _head;
+            var items = new List<string>();
+            
+            while (current != null) {
+                items.Add(current.Val?.ToString()?? "null");
+                current = current.Next;
+            }
+
+            return $"Head -> {string.Join(" -> ", items)}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,45 @@
-﻿
+﻿using DSAPlayground.CoreDataStructure;
+using DSAPlayground.Algorithms;
+
 namespace DSAPlayground
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 3, 11, 5, 2, 7, 2, 6, 10, 1, 15, 9, 14 };
+            var queue = new QueueLinked<string>();
 
-            QuickSort.Sort(arr);
-            foreach (int num in arr) {
-                Console.WriteLine(num);
-            }
-            Console.WriteLine(BinarySearch.Search(arr, 15));
+            Console.WriteLine("IsEmpty: " + queue.IsEmpty);
+            queue.Enqueue("Donnie");
+            queue.Enqueue("Raihan");
+            queue.Enqueue("Zaki");
+
+            Console.WriteLine("Queue: " + queue);
+            Console.WriteLine("Peek: " + queue.Peek());
+            Console.WriteLine("Dequeue: " + queue.Dequeue());
+            Console.WriteLine("Queue after Dequeue: " + queue);
+
+            queue.Clear();
+            Console.WriteLine("Queue after Clear: " + queue);
+            Console.WriteLine("IsEmpty: " + queue.IsEmpty);
+
+        }
+    }
+
+    public class Vehicle
+    {
+        private string _model;
+        private string _make;
+
+        public Vehicle(string model, string make)
+        {
+            _model = model;
+            _make = make;
+        }
+
+        public override string ToString()
+        {
+            return $"({_model}, {_make})";
         }
     }
 }
